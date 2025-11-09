95db6075d57130670773a89e4bb0935385128fa9
Imanol Manero <imanolmanero@gmail.com>
2025-11-09 22:24:15 +0100

    mejoras

--- Practica2v2/script.js
+++ Practica2v2/script.js
@@ -122,3 +122,43 @@ function eliminarUnProducto(event){
 
 añadirProductoButton.addEventListener("click", añadirProducto)
 vaciarListaButton.addEventListener("click",vaciarlista)
+/*
+// ===============================
+// VALIDAR MATRÍCULA ANTIGUA
+// ===============================
+
+// ^ y $ indican inicio y fin
+// [A-Z]{1,2}  → 1 o 2 letras iniciales
+// -[0-9]{4}-  → guion + 4 números + guion
+// ([B-D]|[F-H]|[J-N]|[P-T]|[V-Z]){1,3}
+//              → 1 a 3 letras permitidas (sin vocales ni Ñ)
+
+let m = prompt("Matrícula vieja");
+
+let expreg = /^[A-Z]{1,2}-[0-9]{4}-([B-D]|[F-H]|[J-N]|[P-T]|[V-Z]){1,3}$/;
+
+if (expreg.test(m))
+    alert("La matrícula es correcta");
+else
+    alert("La matrícula NO es correcta");
+
+
+
+
+// ===============================
+// VALIDAR CONTRASEÑA SEGURA
+// ===============================
+
+// ^ y $  → inicio y fin
+// (?=.*[a-z])      → mínimo una minúscula
+// (?=.*[A-Z])      → mínimo una mayúscula
+// (?=.*\d)         → mínimo un número
+// (?=.*[^A-Za-z\d])→ mínimo un carácter especial
+// .{10,}           → mínimo 10 caracteres en total
+
+const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{10,}$/;
+
+function esPasswordSegura(password) {
+    return regexPassword.test(password);
+}
+*/
